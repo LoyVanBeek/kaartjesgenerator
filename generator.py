6dffe16d1cc0273123761d42f98263e6389c0a9a
Loy van Beek <loy.vanbeek@mojin-robotics.de>
2022-04-08 14:41:39 +0200

    Python3 ready

--- generator.py
+++ generator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import sys
+from math import ceil
 from collections import OrderedDict
-SIZE = (918,305)  # (width, height)
+SIZE = (200, 200)  # (width, height)
 
 def main(amount_mapping, border_colored=True, title=None):
     yield """<html>
@@ -18,10 +19,9 @@ def main(amount_mapping, border_colored=True, title=None):
 
 def generate_table(amount_mapping, border_colored=True):
     cells = generate_cards(amount_mapping, border_colored)
-    #import ipdb; ipdb.set_trace()
     total = sum(amount_mapping.values())
-    columns = 1
-    rows = (total/columns)+1
+    columns = 6
+    rows = ceil((total/columns)+1)
 
     yield "\n\t\t<table>"
     for rowno in range(rows):
@@ -30,7 +30,7 @@ def generate_table(amount_mapping, border_colored=True):
         for i in range(columns):
             #print rowno, i
             try:
-                cell = cells.next()
+                cell = next(cells)
                 yield "\t\t\t\t<td>"
                 yield cell
                 yield "\t\t\t\t</td>"
@@ -41,8 +41,7 @@ def generate_table(amount_mapping, border_colored=True):
     yield "\t\t</table>"
 
 def generate_cards(amount_mapping, border_colored=True):
-    #import ipdb; ipdb.set_trace()
-    for imagepath, amount in amount_mapping.iteritems():
+    for imagepath, amount in amount_mapping.items():
         imagecode = """<img src="{0}" height="{2}" width="{1}"/>""".format(imagepath, SIZE[0], SIZE[1])
         for i in range(amount):
             height,width = SIZE
@@ -72,6 +71,6 @@ if __name__ == "__main__":
         parts = amountstr.split(":")
         amount_mapping[parts[0]] =int(parts[1])
 
-    with file(filename, "w+") as f:
+    with open(filename, "w+") as f:
         for html in main(amount_mapping, border_colored=True):
             f.write(html)
